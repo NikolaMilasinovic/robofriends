cdef6cf4c19bcc87aeb0b63be0e8cabe5e03d60a
Developerweights <nikola.milasinovic07@gmail.com>
2019-07-24 09:24:30 +0200

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import SearchBox from '../components/SearchBox';
+import CardLIst from '../components/CardList';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return{
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+class App extends Component {
+	
+
+	componentDidMount() {
+		this.props.onRequestRobots();	
+	}
+
+	// onSearchChange = (event) =>{
+	// 	this.setState({searchfield: event.target.value})
+	// }
+	render(){
+		const {searchField, onSearchChange, robots, isPending} = this.props;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField)
+		})
+		return isPending ?
+			 <h1>Loading</h1>:
+			(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={onSearchChange}/>
+					<Scroll>
+						<ErrorBoundry>
+							<CardLIst robots={filteredRobots}/>
+						</ErrorBoundry>
+					</Scroll>
+				</div>
+			
+			)
+		
+	}
+	
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
